99d6e51e48a11f0abbe3d4fbf3c0e3bf7d12d071
ALentini222 <77400292+ALentini222@users.noreply.github.com>
2021-12-01 19:30:23 -0500

    Unit testing started with yet another new project started due to corrupted files.  Oh the joys :/

---
+++ Comp2048-Assignment-Andreas1141007-Test/GamesControllerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Comp2048_Assignment_Andreas1141007_Test
+{
+    [TestClass]
+    public class GamesControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
